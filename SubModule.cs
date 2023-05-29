9e0c38d2eb430db29bf6dcf7c46fcc7b008aed59
qunosteve <stephencano56@hotmail.com>
2023-05-29 14:55:09 -0500

    Created basic BUTR template.

---
+++ SubModule.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace tasamphitheatre
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
