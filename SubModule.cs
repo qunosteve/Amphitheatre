ee85122b45b56c0ab9cd8c49c759ff4060ae4957
qunosteve <stephencano56@hotmail.com>
2023-06-03 15:13:50 -0500

    Establishing Game mode if statement.

--- SubModule.cs
+++ SubModule.cs
@@ -8,6 +8,7 @@ namespace tasamphitheatre
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
+            Console.WriteLine("Welcome to the Ape Society!");
 
         }
 
@@ -22,5 +23,25 @@ namespace tasamphitheatre
             base.OnBeforeInitialModuleScreenSetAsRoot();
 
         }
-    }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (game.GameType is Campaign)
+            {
+                // Code to modify campaign behavior goes here
+                // For example, adding custom logic or tweaking game mechanics
+            }
+
+            if (game.GameType is Singleplayer)
+            {
+                // Code to modify singleplayer behavior goes here
+            }
+
+            if (game.GameType is Multiplayer)
+            {
+                // Code to modify multiplayer behavior goes here
+            }
+        }
 }
